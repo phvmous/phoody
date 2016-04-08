0cdc51819ea7c4f7280bfc6f9d52adf3c28c7365
Thomas Pham <phvmous@gmail.com>
2016-04-08 10:10:11 +0700

    Added a if / else statement for initialize phoody
    May need some revising, may opt to use a switch if the options get bigger.

--- phoody.js
+++ phoody.js
@@ -1,22 +1,38 @@
-<<<<<<< HEAD
-var foodPlaces = prompt("List 7 food places you like to eat frequently (Please seperate each place with a ,)").split(",");
-
-
-=======
 
 /*	phoody 1.0 by Thomas Pham
 *	tom@phamous.co
 *
 *	Licensed under the MIT License
 *	
-*	Visit github.com/phvmous/phoody for documentation and updates
+*	Visit https://github.com/phvmous/phoody for documentation and updates.
 */
 
 
+// starts with a prompt
+var message = prompt("How would you like phoody to help you? Type 'A' to pick a type of food for me. Or type 'B' to save a list of places to eat").toUpperCase();
+
+var messageChoice = function (choice) {
+	if (choice === "A") {
+		alert("phoody will randomly choose a type of food for you to eat, click ok to begin.");
+		pickPlace();
+	}
+	else if (choice === "B") {
+		alert("phoody will store places you like to eat at and choose randomly, click ok to begin.");
+		favoritePlaces();
+	}
+	else (choice !== "A" && choice !== "B"){
+		alert("Please pick a valid choice, more will come later");
+		console.log(message);
+		messageChoice(message);
+	}
+};
+
+
+// stores user choices into an array.
 var foodPlaces = [];
 
-var placesToEat = function () {
-	var eatWhere = prompt("Enter in as many places you like to eat at (seperated by a ,)").toUpperCase().split(",");
+var favoritePlaces = function () {
+	var eatWhere = prompt("Enter in as many places you like to eat at (seperated by a ,)").split(",");
 	foodPlaces.push(eatWhere);
 	for(var i = 0; i < foodPlaces.length; i++) {
 		console.log(foodPlaces[i]);
@@ -26,4 +42,3 @@ var placesToEat = function () {
 
 
 
->>>>>>> origin/master
